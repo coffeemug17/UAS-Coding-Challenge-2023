d741cdd3e2d6ca7b21217cf8dc0e8f998c6a5eaa
coffeemug17 <mitulpandey2@gmail.com>
2023-09-17 23:14:44 -0700

    handleClick for MultiplicationTable.jsx

--- src/Components/MultiplicationPanel/MultiplicationPanel.jsx
+++ src/Components/MultiplicationPanel/MultiplicationPanel.jsx
@@ -2,11 +2,17 @@ import { useState } from "react";
 import "./MultiplicationPanel.css";
 
 export default function MultiplicationPanel() {
+    const [history, setHistory] = useState([]);
     const [currProd, setCurrProd] = useState(null);
 
     const handleClick = (row, col) => {
         const product = row * col;
         setCurrProd(product);
+
+        //  Add the product to the history if it's not already there
+        if (!history.some((prod) => prod.row === row && prod.col === col)) {
+            setHistory([...history, { row, col, product }]);
+        }
     }
 
     return (
